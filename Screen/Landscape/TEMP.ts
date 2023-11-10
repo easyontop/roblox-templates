6e437a7713098f86e23a052ea461e5ec706bb4e6
RbxScripts <88426249+easyontop@users.noreply.github.com>
2023-11-10 11:57:54 +0800

    Create TEMP.ts

---
+++ Screen/Landscape/TEMP.ts
@@ -0,0 +1,10 @@
+/*
+⚠️ | Script / LocalScript (<...>.server.ts[x] / <...>.client.ts[x])
+❌ | ModuleScript (<...>.ts[x])
+*/
+
+// npm install @rbxts/services -D --silent
+import { StarterGui: starterGui } from "@rbxts/services";
+// Replace with "game.GetService" if you want. 
+// https://roblox-ts.com/
+starterGui.ScreenOriantation = Enum.ScreenOrientation.LandscapeSensor
